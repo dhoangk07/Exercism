ecbb92190d8482c3b80502a1d76b63949891f11b
HoangDinh <dhoangk07@gmail.com>
2018-10-06 12:48:25 +0700

    Add enum for exercise and solution model.

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,3 +1,4 @@
 class Exercise < ApplicationRecord
   has_may :solutions
+  enum difficulty: { easy: 1, medium: 2, hard: 3}
 end
--- app/models/solution.rb
+++ app/models/solution.rb
@@ -1,3 +1,4 @@
 class Solution < ApplicationRecord
   belongs_to :exercise
+  enum status: { in_progress: 1, unlocked: 2, completed: 3, locked: 4}
 end
