e8163606f4ec42a89381cb21d374c6b81c38ca5d
HoangDinh <dhoangk07@gmail.com>
2018-10-06 17:24:16 +0700

    change format of enum

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,5 +1,5 @@
 class Exercise < ApplicationRecord
   has_many :solutions
   mount_uploader :avatar, AvatarUploader
-  enum difficulty: { easy: 1, medium: 2, hard: 3}
+  enum difficulty: [:easy, :medium, :hard]
 end
