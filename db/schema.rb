a0aaab7aa1cb28f481ddf65c12b8b0b3d3f5c013
HoangDinh <dhoangk07@gmail.com>
2018-10-06 12:43:41 +0700

    Create model solution.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_06_054124) do
+ActiveRecord::Schema.define(version: 2018_10_06_054314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2018_10_06_054124) do
   create_table "exercises", force: :cascade do |t|
     t.string "title"
     t.string "difficulty"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "solutions", force: :cascade do |t|
+    t.string "status"
+    t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
