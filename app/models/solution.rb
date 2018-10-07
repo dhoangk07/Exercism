febe2764e5b7ac5db913befc3a5bab396922d31c
HoangDinh <dhoangk07@gmail.com>
2018-10-07 13:37:41 +0700

    Recorrect enum for solution model

--- app/models/solution.rb
+++ app/models/solution.rb
@@ -2,4 +2,5 @@ class Solution < ApplicationRecord
   belongs_to :exercise
   belongs_to :user
   enum status: { in_progress: 1, unlocked: 2, completed: 3, locked: 4}
+  enum status: [:locked, :in_progress, :completed]
 end
