f6b7e54618b2f64e7a50f7ba10b55b5c607fc0db
HoangDinh <dhoangk07@gmail.com>
2018-10-08 23:12:10 +0700

    Add method to generate path of exercise

--- app/helpers/exercises_helper.rb
+++ app/helpers/exercises_helper.rb
@@ -1,3 +1,7 @@
 module ExercisesHelper
   include ActsAsTaggableOn::TagsHelper
+
+  def generate_exercise_path(attribute)
+    exercises_path(request.query_parameters.merge(attribute))
+  end  
 end
