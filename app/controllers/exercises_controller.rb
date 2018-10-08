7e36c1ed53b5a8e492cb4f97253707914bb90a82
HoangDinh <dhoangk07@gmail.com>
2018-10-08 12:48:11 +0700

    1. Refactor code for index view of exercise 2. Create sort for exercise depend on difficulty, topic, status field

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -2,7 +2,28 @@ class ExercisesController < ApplicationController
 before_action :set_exercise, only: %i[edit update show destroy]
   def index
     if params[:tag]
-      @exercises = Exercise.tagged_with(params[:tag])
+     #topic 
+      tag_ids = Tag.where(name: params[:tag]).pluck(:id)
+      tagging_ids = Tagging.where(id: tag_ids).pluck(:exercise_id)
+      @exercises = Exercise.where(id: tagging_ids)
+      # @exercises = Exercise.tagged_with(params[:tag])
+    #difficulty
+    elsif params[:filter] == 'easy'
+      @exercises= Exercise.where(difficulty: 'easy')
+    elsif params[:filter] == 'medium'
+      @exercises= Exercise.where(difficulty: 'medium')
+    elsif params[:filter] == 'hard'
+      @exercises= Exercise.where(difficulty: 'hard')
+    #status
+    elsif params[:filter] == 'lock'
+      exercise_ids = Solution.where(status: 'lock').where(user_id: current_user.id).pluck(:exercise_id)
+      @exercises = Exercise.where(id: exercise_ids)
+    elsif params[:filter] == 'in_progress'
+      exercise_ids = Solution.where(status: 'in_progress').where(user_id: current_user.id).pluck(:exercise_id)
+      @exercises = Exercise.where(id: exercise_ids)
+    elsif params[:filter] == 'completed'
+      exercise_ids = Solution.where(status: 'completed').where(user_id: current_user.id).pluck(:exercise_id)
+      @exercises = Exercise.where(id: exercise_ids)
     else
       @exercises = Exercise.all
     end
@@ -31,6 +52,7 @@ before_action :set_exercise, only: %i[edit update show destroy]
 
   def show
     @solution = Solution.where(user_id: current_user.id, exercise_id: @exercise.id).first
+    @tags = Tag.all
   end
 
   def destroy
