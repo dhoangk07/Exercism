8da1d93c1f59ff241b648b8b53374f459aa97b0e
HoangDinh <dhoangk07@gmail.com>
2018-10-09 08:34:58 +0700

    Add db/seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-Exercise.delete_all
-#==> create user
-# 10.times do |n|
-#   name = Faker::FunnyName.two_word_name
-#   email = Faker::Internet.email
-#   User.create!(name: name, email: email)  
-# end
 
-#==> create exercise
+Exercise.delete_all
+# Tag.delete_all
+Tagging.delete_all
+Solution.delete_all
+
 90.times do |n|
   title = Faker::Food.dish 
   difficulty = Exercise.difficulties.keys.sample
   introduction = Faker::Lorem.paragraph(500)
   Exercise.create!(title: title, difficulty: difficulty, introduction: introduction)
 end
-#==> create solution
-# users = User.all
-# 90.times do |m|
-#   content = Faker::Lorem.sentences(5)
-#   users.each{ |user| user.solutions.create!(content: content)}
+
+# 20.times do |n|
+#   name = Faker::Food.vegetables
+#   Tag.create!(name: name)
 # end
 
+300.times do |n|
+  name = Faker::Friends.character
+  email = Faker::Internet.email 
+  password = Faker::Internet.password
+  password_confirmation = password
+  User.create!(name: name, email: email, password: password, password_confirmation: password_confirmation)
+end
 
+190.times do |n|
+  Tagging.create!(tag_id: Tag.pluck(:id).sample, exercise_id: Exercise.pluck(:id).sample)
+end
+
+exercises = Exercise.take(30)
+30.times do |n|
+  content = Faker::Lorem.paragraph(150)
+  status = Solution.statuses.keys.sample
+  exercises.each{|exercise| exercise.solutions.create!(content: content, status: status, user_id: User.pluck(:id).sample)}
+end
 
 
 
